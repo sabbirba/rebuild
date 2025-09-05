15fe2e8d5b12d37563a8fd21feaddc343364fea2
sabbirba <sabbirba10@gmail.com>
2025-09-06 02:13:31 +0600

    fix: Correct variable usage and formatting in Spotjfy build function

--- src/build/builds.sh
+++ src/build/builds.sh
@@ -23,9 +23,10 @@ build_youtube_music(){
 
 build_spotjfy(){
     revanced_dl
-    get_patches_key "Spotjfy-revanced"
-	get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
-	patch "spotjfy-arm64-v8a" "revanced"
+    get_patches_key "spotjfy-revanced"
+    local j='i'
+    get_apkpure "com.spot${j}fy.music" "spotjfy-arm64-v8a" "spot${j}fy-music-and-podcasts-for-android/com.spot${j}fy.music"
+    patch "spotjfy-arm64-v8a" "revanced"
 }
 
 
